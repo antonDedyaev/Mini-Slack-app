69cac33695d8a8dd18b778f25621f9bb98a67293
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2022-10-21 17:06:47 +0300

    add custom hooks and backend routes

---
+++ frontend/src/hooks/PrivateRoute.jsx
@@ -0,0 +1,13 @@
+import { useLocation, Navigate } from 'react-router-dom';
+import useAuth from './useAuth';
+
+const PrivateRoute = ({ children }) => {
+  const location = useLocation();
+  const auth = useAuth();
+
+  return (
+    auth.user ? children : <Navigate to="/login" state={{ from: location }} />
+  );
+};
+
+export default PrivateRoute;
