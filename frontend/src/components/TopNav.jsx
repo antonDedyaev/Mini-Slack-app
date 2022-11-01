fa872a938d616c7f08447079e0810bedffd98a99
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2022-11-01 11:30:19 +0300

    add logout button

--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
 
-import { Navbar, Container } from 'react-bootstrap';
+import { Button, Navbar, Container } from 'react-bootstrap';
+import useAuth from '../hooks/useAuth';
 
-const TopNav = () => (
-  <Navbar className="shadow-sm" expand="lg" bg="white">
-    <Container>
-      <Navbar.Brand href="/">Hexlet Chat</Navbar.Brand>
-    </Container>
-  </Navbar>
-);
+const TopNav = () => {
+  const { user, logOut } = useAuth();
+  const logoutButton = (
+    <Button
+      type="button"
+      variant="primary"
+      onClick={logOut}
+    >
+      Выйти
+    </Button>
+  );
+
+  return (
+    <Navbar className="shadow-sm" expand="lg" bg="white">
+      <Container>
+        <Navbar.Brand href="/">Hexlet Chat</Navbar.Brand>
+        { user ? logoutButton : null}
+      </Container>
+    </Navbar>
+  );
+};
 
 export default TopNav;
