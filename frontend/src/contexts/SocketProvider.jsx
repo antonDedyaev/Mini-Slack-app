1dc03e4ee9625087f70100b138a2ac1fae3531f2
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2022-11-02 12:31:52 +0300

    move sockets to init file

--- frontend/src/contexts/SocketProvider.jsx
+++ frontend/src/contexts/SocketProvider.jsx
@@ -1,13 +1,11 @@
 /* eslint-disable react/jsx-no-constructed-context-values */
 import { createContext } from 'react';
-import { useDispatch } from 'react-redux';
-import { messageAdded } from '../slices/messagesSlice';
-import { channelAdded, channelRemoved, channelRenamed } from '../slices/channelsSlice';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.min.css';
 
 export const SocketContext = createContext({});
 
 export const SocketProvider = ({ socket, children }) => {
-  const dispatch = useDispatch();
   const addMessage = (body, channelId, username) => {
     socket.emit('newMessage', { body, channelId, username }, (response) => {
       if (response.status !== 'ok') {
@@ -16,45 +14,32 @@ export const SocketProvider = ({ socket, children }) => {
     });
   };
 
-  socket.on('newMessage', (payload) => {
-    dispatch(messageAdded(payload));
-  });
-
   const addChannel = (name) => {
     socket.emit('newChannel', { name }, (response) => {
       if (response.status !== 'ok') {
         console.log('Bad request!');
       }
+      toast.success('Канал создан');
     });
   };
-
-  socket.on('newChannel', (payload) => {
-    dispatch(channelAdded(payload));
-  });
 
   const removeChannel = (id) => {
     socket.emit('removeChannel', { id }, (response) => {
       if (response.status !== 'ok') {
         console.log('Bad request!');
       }
+      toast.success('Канал удалён');
     });
   };
-
-  socket.on('removeChannel', (payload) => {
-    dispatch(channelRemoved(payload));
-  });
 
   const renameChannel = (id, name) => {
     socket.emit('renameChannel', { id, name }, (response) => {
       if (response.status !== 'ok') {
         console.log('Bad request!');
       }
+      toast.success('Канал переименован');
     });
   };
-
-  socket.on('renameChannel', (payload) => {
-    dispatch(channelRenamed(payload));
-  });
 
   const chatAPI = {
     addMessage,
