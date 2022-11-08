751b7de3b2eb4e2dd3d5245514974caacfc1b85d
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2022-11-08 18:01:31 +0300

    move dict loading to initialization stage

--- frontend/src/utils/profanityFilter.js
+++ frontend/src/utils/profanityFilter.js
@@ -1,7 +1,3 @@
 import filter from 'leo-profanity';
 
-filter.add(filter.getDictionary('en'));
-filter.add(filter.getDictionary('fr'));
-filter.add(filter.getDictionary('ru'));
-
 export default (text) => filter.clean(text);
