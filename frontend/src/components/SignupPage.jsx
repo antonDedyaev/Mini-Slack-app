c8cd7407cc732c6df03aaf9fe6836bff571ec72d
Aintdead86 <96647913+antonDedyaev@users.noreply.github.com>
2022-11-08 17:49:17 +0300

    add disabling logic for submit form and button

--- frontend/src/components/SignupPage.jsx
+++ frontend/src/components/SignupPage.jsx
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import React, { useState, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { useFormik } from 'formik';
@@ -14,6 +14,7 @@ import routes from '../utils/routes';
 
 const SignupPage = () => {
   const { t } = useTranslation();
+  const [isLoading, setIsLoading] = useState(false);
   const inputRef = useRef();
   const auth = useAuth();
   const navigate = useNavigate();
@@ -44,8 +45,10 @@ const SignupPage = () => {
     validationSchema,
     onSubmit: async ({ username, password }) => {
       try {
+        setIsLoading(true);
         const response = await axios.post(routes.signupPath(), { username, password });
         auth.logIn(response.data);
+        setIsLoading(false);
         navigate('/');
       } catch (err) {
         switch (err.message) {
@@ -74,57 +77,69 @@ const SignupPage = () => {
                 <img src={signupImg} className="rounded-circle" alt={t('pages.signup.title')} />
               </div>
               <Form onSubmit={formik.handleSubmit} className="w-50">
-                <h1 className="text-center mb-4">{t('pages.signup.title')}</h1>
-                <Form.Group className="form-floating mb-3">
-                  <Form.Control
-                    placeholder={t('formWarnings.nameLength')}
-                    name="username"
-                    autoComplete="username"
-                    required
-                    id="username"
-                    onChange={formik.handleChange}
-                    value={formik.values.username}
-                    ref={inputRef}
-                    isInvalid={(formik.errors.username && formik.touched.username)
-                        || formik.errors.existingUser}
-                  />
-                  <Form.Label htmlFor="username">{t('pages.signup.username')}</Form.Label>
-                  <Form.Control.Feedback placement="right" type="invalid" tooltip>{formik.errors.username}</Form.Control.Feedback>
-                </Form.Group>
-                <Form.Group className="form-floating mb-3">
-                  <Form.Control
-                    placeholder={t('formWarnings.minPasswordLength')}
-                    name="password"
-                    aria-describedby="passwordHelpBlock"
-                    required
-                    autoComplete="new-password"
-                    type="password"
-                    id="password"
-                    onChange={formik.handleChange}
-                    value={formik.values.password}
-                    isInvalid={(formik.errors.password && formik.touched.password)
-                        || formik.errors.existingUser}
-                  />
-                  <Form.Control.Feedback type="invalid" tooltip>{formik.errors.password}</Form.Control.Feedback>
-                  <Form.Label className="form-label" htmlFor="password">{t('pages.signup.password')}</Form.Label>
-                </Form.Group>
-                <Form.Group className="form-floating mb-4">
-                  <Form.Control
-                    placeholder={t('formWarnings.notMatched')}
-                    name="confirmPassword"
-                    required
-                    autoComplete="new-password"
-                    type="password"
-                    id="confirmPassword"
-                    onChange={formik.handleChange}
-                    value={formik.values.confirmPassword}
-                    isInvalid={(formik.errors.confirmPassword && formik.touched.confirmPassword)
-                        || formik.errors.existingUser}
-                  />
-                  <Form.Control.Feedback type="invalid" tooltip>{formik.errors.confirmPassword || formik.errors.existingUser}</Form.Control.Feedback>
-                  <Form.Label className="form-label" htmlFor="confirmPassword">{t('pages.signup.confirmPassword')}</Form.Label>
-                </Form.Group>
-                <Button variant="outline-primary" type="submit" className="w-100">{t('pages.signup.signupBtn')}</Button>
+                <fieldset disabled={isLoading}>
+                  <h1 className="text-center mb-4">{t('pages.signup.title')}</h1>
+                  <Form.Group className="form-floating mb-3">
+                    <Form.Control
+                      placeholder={t('formWarnings.nameLength')}
+                      name="username"
+                      autoComplete="username"
+                      required
+                      id="username"
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.username}
+                      ref={inputRef}
+                      isInvalid={(formik.errors.username && formik.touched.username)
+                          || formik.errors.existingUser}
+                    />
+                    <Form.Label htmlFor="username">{t('pages.signup.username')}</Form.Label>
+                    <Form.Control.Feedback placement="right" type="invalid" tooltip>{formik.errors.username}</Form.Control.Feedback>
+                  </Form.Group>
+                  <Form.Group className="form-floating mb-3">
+                    <Form.Control
+                      placeholder={t('formWarnings.minPasswordLength')}
+                      name="password"
+                      aria-describedby="passwordHelpBlock"
+                      required
+                      autoComplete="new-password"
+                      type="password"
+                      id="password"
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.password}
+                      isInvalid={(formik.errors.password && formik.touched.password)
+                          || formik.errors.existingUser}
+                    />
+                    <Form.Control.Feedback type="invalid" tooltip>{formik.errors.password}</Form.Control.Feedback>
+                    <Form.Label className="form-label" htmlFor="password">{t('pages.signup.password')}</Form.Label>
+                  </Form.Group>
+                  <Form.Group className="form-floating mb-4">
+                    <Form.Control
+                      placeholder={t('formWarnings.notMatched')}
+                      name="confirmPassword"
+                      required
+                      autoComplete="new-password"
+                      type="password"
+                      id="confirmPassword"
+                      onChange={formik.handleChange}
+                      onBlur={formik.handleBlur}
+                      value={formik.values.confirmPassword}
+                      isInvalid={(formik.errors.confirmPassword && formik.touched.confirmPassword)
+                          || formik.errors.existingUser}
+                    />
+                    <Form.Control.Feedback type="invalid" tooltip>{formik.errors.confirmPassword || formik.errors.existingUser}</Form.Control.Feedback>
+                    <Form.Label className="form-label" htmlFor="confirmPassword">{t('pages.signup.confirmPassword')}</Form.Label>
+                  </Form.Group>
+                  <Button
+                    variant="outline-primary"
+                    type="submit"
+                    className="w-100"
+                    disabled={isLoading}
+                  >
+                    {t('pages.signup.signupBtn')}
+                  </Button>
+                </fieldset>
               </Form>
             </div>
           </div>
